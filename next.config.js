959f1aaf52f6a95091304d063f563f09c4af5aae
newtalentxp <newtalentxp@126.com>
2022-12-21 01:02:38 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 console.log(process.version);
-console.log('NODE_ENV: ', process.env.NODE_ENV);
+console.log('NODE_ENV: ', process.env.NODE_ENV, isProd);
 console.log('STATIC_URL: ', process.env.STATIC_URL);
 console.log('AUTHOK_SECRET: ', process.env.AUTHOK_SECRET);
 
