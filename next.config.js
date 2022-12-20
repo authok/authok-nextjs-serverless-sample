18869cfb423435ef06dc6c6de8a01e91bc2fe74d
newtalentxp <newtalentxp@126.com>
2022-12-20 19:52:00 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 console.log(process.version);
+console.log('STATIC_URL: ', process.env.STATIC_URL);
 
 module.exports = {
+  env: {
+    STATIC_URL: isProd ? process.env.STATIC_URL : '',
+  },
+  assetPrefix: isProd ? process.env.STATIC_URL : '',
   poweredByHeader: false
 };
