598f47148cd342110809f99bda9bd64de515e34e
newtalentxp <newtalentxp@126.com>
2022-12-20 23:58:24 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,7 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import React from 'react';
+import styles from '../styles/index.module.css'
 import { useUser } from '@authok/nextjs-authok/client';
 
 import Layout from '../components/layout';
@@ -8,8 +11,16 @@ export default function Home() {
 
   return (
     <Layout>
-      <h1>Next.js and Authok Example</h1>
+      <Head>
+        <title>Serverless - Next.js</title>
+        <meta name="description" content="Serverless Next.js 应用" />
+        <meta name="keywords" content="next,next.js,serverless,无服务" />
+        <link rel="icon" href={`${process.env.STATIC_URL || ''}/favicon.ico`} />
+      </Head>
+      <h1 className={styles.title}>Next.js and Authok Example</h1>
+      <p>STATIC_URL: {process.env.STATIC_URL}</p>
 
+      <Image src="/authok-badge.png" alt="authok" width={200} height={200} />
       {isLoading && <p>加载登录信息...</p>}
 
       {error && (
